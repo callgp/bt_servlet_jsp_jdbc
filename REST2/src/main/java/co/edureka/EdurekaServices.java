a8e54ece518a87bb283058eba2fbf854345005f9
callgp <callgp@gmail.com>
2020-11-22 16:37:20 -0500

    spring

--- REST2/src/main/java/co/edureka/EdurekaServices.java
+++ REST2/src/main/java/co/edureka/EdurekaServices.java
@@ -1,8 +1,12 @@
 package co.edureka;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 @Path("edureka")
 public class EdurekaServices {
@@ -23,6 +27,30 @@ public class EdurekaServices {
 		public String nriService(){
 			return "<h2>nri customer service center</h2>";
 		}
+		
+		
+		@GET
+		@Path("{name}/{course}")
+		@Produces("text/html")
+		public String enquiry(@PathParam("name") String name,@PathParam("course") String course){
+			return " customer name" +name+ "course is" +course;
+		}
+		
+		
+		@GET
+		@Path("/enquiry")
+		@Produces("text/html")
+		public String enquiry1(@QueryParam("name") String name,@QueryParam("course") String course){
+			return " customer name" +name+ "course is" +course;
+		}
+		
+		@POST
+		@Path("/registerCustomer")
+		@Produces("text/html")
+		public String register(@FormParam("name") String name,@FormParam("age") String age){
+			return " customer name" +name+ "course is" +age;
+		}
+		
 //	}
 
 }
--- REST2/src/main/webapp/index.jsp
+++ REST2/src/main/webapp/index.jsp
@@ -3,5 +3,6 @@
 <h2>Hello World!</h2>
 <a href="rest/edureka">local service</a>
 <a href="rest/edureka/nri">remote service</a>
+<a href="registration.html">register  service</a>
 </body>
 </html>
