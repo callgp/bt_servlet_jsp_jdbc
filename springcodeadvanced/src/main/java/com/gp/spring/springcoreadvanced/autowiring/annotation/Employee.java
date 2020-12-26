0f482b954d961d3d19865c40577f0ec2fdc81173
callgp <callgp@gmail.com>
2020-12-26 17:41:03 -0500

    annotation for autowire

---
+++ springcodeadvanced/src/main/java/com/gp/spring/springcoreadvanced/autowiring/annotation/Employee.java
@@ -0,0 +1,28 @@
+package com.gp.spring.springcoreadvanced.autowiring.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+
+
+
+	/*public Employee(Address address) {
+		super();
+		this.address = address;
+	}*/
+	private Address address;
+	
+	public Address getAddress() {
+		return address;
+	}
+	@Autowired
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Employee [ address=" + address + "]";
+	}
+
+
+}
